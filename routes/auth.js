a0810df55bc31ab020b6c7a983e30b823f17abcc
Pedro Henrique <pedro.ferrari1@aluno.unip.br>
2025-05-09 19:46:48 -0300

    Removes Docker instructions from auth route
    Removes Docker-related commands from the auth route file.
    These instructions are more appropriate for a Dockerfile.

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,10 @@
-# Diretório de trabalho dentro do container
 WORKDIR /app
 
-# Copia os arquivos
 COPY package*.json ./
 RUN npm install
 
 COPY . .
 
-# Porta que o app escuta
 EXPOSE 3001
 
-# Start
 CMD ["node", "server.js"]
